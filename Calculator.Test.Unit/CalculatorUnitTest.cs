bd30337b0aba406e36ceae90abedb1439ed17954
Moheb420 <68193758+Moheb420@users.noreply.github.com>
2022-09-26 19:37:39 +0200

    Feature, exceptions and new tests are added

--- Calculator.Test.Unit/CalculatorUnitTest.cs
+++ Calculator.Test.Unit/CalculatorUnitTest.cs
@@ -1,7 +1,11 @@
+using Calculator_;
+using Microsoft.VisualStudio.CodeCoverage;
+
 namespace Calculator.Test.Unit
 {
     public class Tests
     {
+
         [SetUp]
         public void Setup()
         {
@@ -15,6 +19,25 @@ namespace Calculator.Test.Unit
 
         }
 
+
+        [Test]
+        public void AddNumberWithTwoParameterWithExceptioinThrowFirstParameter()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            Assert.Throws<Exception>(() => calc.Add(2, -2));
+
+        }
+
+        [Test]
+        public void AddNumberWithTwoParameterWithExceptioinThrow2()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            Assert.Throws<Exception>(() => calc.Add(-2, 2));
+
+        }
+        //-------------------------------------------------------------------------------------------------------
+
+
         [Test]
         public void SubstractWithTwoParameterr()
         {
@@ -24,29 +47,105 @@ namespace Calculator.Test.Unit
 
         }
 
+
+
+        [Test]
+        public void SubtractWithTwoParameterWithExceptioinThrowFirstParameter()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            Assert.Throws<Exception>(() => calc.Subtract(2, -8));
+
+        }
+
+        [Test]
+        public void SubtractNumberWithTwoParameterWithExceptioinSecondParameter()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            Assert.Throws<Exception>(() => calc.Subtract(-7, 2));
+
+        }
+
+
+
+
+        //-------------------------------------------------------------------------------------------------------
+
         [Test]
         public void MultiplyWithTwoParameter()
         {
 
             Calculator_.Calculator calc = new Calculator_.Calculator();
-            Assert.That(calc.Multiply(2,4), Is.EqualTo(8));
+            Assert.That(calc.Multiply(2, 4), Is.EqualTo(8));
 
         }
+
+        [Test]
+        public void MultiplyWithTwoParameterWithExceptioinThrowFirstParameter()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            Assert.Throws<Exception>(() => calc.Multiply(5, -66));
+
+        }
+
+        [Test]
+        public void MultiplyNumberWithTwoParameterWithExceptioinSecondParameter()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            Assert.Throws<Exception>(() => calc.Multiply(-1, 9));
+
+        }
+
+
+        //-------------------------------------------------------------------------------------------------------
+
 
         [Test]
         public void PowerWithTwoParameter()
         {
 
             Calculator_.Calculator calc = new Calculator_.Calculator();
-            Assert.That(calc.Power(2,3), Is.EqualTo(8));
+            Assert.That(calc.Power(2, 3), Is.EqualTo(8));
+        }
+        [Test]
+        public void PowerWithTwoParameterWithExceptioinThrowFirstParameter()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            Assert.Throws<Exception>(() => calc.Power(5, -6));
+
         }
 
+        [Test]
+        public void PowerNumberWithTwoParameterWithExceptioinSecondParameter()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            Assert.Throws<Exception>(() => calc.Power(-5, 9));
+
+        }
+
+
+        //-------------------------------------------------------------------------------------------------------
         [Test]
         public void DivideWithTwoParameter()
         {
 
             Calculator_.Calculator calc = new Calculator_.Calculator();
             Assert.That(calc.Divide(4, 2), Is.EqualTo(2));
+
+        }
+
+        [Test]
+        public void DivideWithTwoParameterWithExceptioinThrowFirstParameter()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            Assert.Throws<DivideByZeroException>(() => calc.Divide(5, 0));
+
+        }
+
+        [Test]
+        public void DivideNumberWithTwoParameterWithExceptioinSecondParameter()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            Assert.Throws<Exception>(() => calc.Divide(-5, 9));
 
         }
 
@@ -62,6 +161,25 @@ namespace Calculator.Test.Unit
         }
 
         [Test]
+        public void AddNumberWithAccumulatorExceptionThrowLessThanZero()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+
+            Assert.Throws<Exception>(() => calc.Add(-2));
+
+        }
+
+        [Test]
+        public void AddNumberWithAccumulatorExceptionThrowNumberIsEqualToZero()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+
+            Assert.Throws<Exception>(() => calc.Add(0));
+
+        }
+
+        //-------------------------------------------------------------------------------------------------------
+        [Test]
         public void SubstractFromAccumulator()
         {
 
@@ -73,6 +191,26 @@ namespace Calculator.Test.Unit
         }
 
         [Test]
+        public void SubtractWithAccumulatorExceptionThrowLessThanZero()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            calc.Add(7);
+            Assert.Throws<Exception>(() => calc.Substract(-2));
+
+        }
+
+        [Test]
+        public void SubtractWithAccumulatorExceptionThrowNumberIsEqualToZero()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            calc.Add(8);
+            Assert.Throws<Exception>(() => calc.Substract(0));
+
+        }
+
+
+        //-------------------------------------------------------------------------------------------------------
+        [Test]
         public void MultiplyWithAccumulator()
         {
 
@@ -83,7 +221,26 @@ namespace Calculator.Test.Unit
 
         }
 
+        [Test]
+        public void MultiplyWithAccumulatorExceptionThrowLessThanZero()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            calc.Add(7);
+            Assert.Throws<Exception>(() => calc.Multiply(-9));
 
+        }
+
+        [Test]
+        public void MultiplyWithAccumulatorExceptionThrowNumberIsEqualToZero()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            calc.Add(5);
+            Assert.Throws<Exception>(() => calc.Multiply(0));
+
+        }
+
+
+        //-------------------------------------------------------------------------------------------------------
         [Test]
         public void DivideWithAccumulator()
         {
@@ -96,6 +253,25 @@ namespace Calculator.Test.Unit
         }
 
         [Test]
+        public void DivideWithAccumulatorExceptionThrowLessThanZero()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            calc.Add(12);
+            Assert.Throws<Exception>(() => calc.Divide(-9));
+
+        }
+
+        [Test]
+        public void DivideWithAccumulatorExceptionThrowNumberIsEqualToZero()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            calc.Add(25);
+            Assert.Throws<DivideByZeroException>(() => calc.Divide(0));
+
+        }
+
+        //-------------------------------------------------------------------------------------------------------
+        [Test]
         public void PowerOfAccumulator()
         {
 
@@ -105,14 +281,74 @@ namespace Calculator.Test.Unit
             Assert.That(calc.accumulator, Is.EqualTo(4));
         }
 
+
+        [Test]
+        public void PowerWithAccumulatorExceptionThrowLessThanZero()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            calc.Add(32);
+            Assert.Throws<Exception>(() => calc.Power(-19));
+
+        }
+
+        [Test]
+        public void PowerWithAccumulatorExceptionThrowNumberIsGreatherThan1000()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            calc.Add(45);
+            Assert.Throws<IndexOutOfRangeException>(() => calc.Power(2000));
+
+        }
+
+        //-------------------------------------------------------------------------------------------------------
+
+
+        [Test]
+        public void SquarRootWithAccumulator()
+        {
+
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            calc.squarRoot(4);
+            Assert.That(calc.accumulator, Is.EqualTo(2));
+        }
+
+
+        [Test]
+        public void SquarRootWithAccumulatorTest1()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            calc.Add(2);
+            Assert.Throws<Exception>(() => calc.squarRoot(0));
+
+        }
+
+        [Test]
+        public void SquarRootWithAccumulatorTest2()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            calc.Add(45);
+            Assert.Throws<Exception>(() => calc.Power(-2));
+
+        }
+        //-------------------------------------------------------------------------------------------------------
+
         [Test]
         public void ClearAccumulator()
         {
-
             Calculator_.Calculator calc = new Calculator_.Calculator();
             calc.Add(12);
             calc.clear();
             Assert.That(calc.accumulator, Is.EqualTo(0));
         }
+        [Test]
+        public void TestAccumalator()
+        {
+            Calculator_.Calculator calc = new Calculator_.Calculator();
+            calc.Add(4);
+            calc.Substract(4);
+            Assert.Throws<Exception>(() => calc.clear());
+
+        }
+        
     }
 }
--- CalculatorNetClasses/Calculator.cs
+++ CalculatorNetClasses/Calculator.cs
@@ -9,35 +9,127 @@ namespace Calculator_
     public class Calculator
     {
 
+
+        private double c = 0;
         public double Add(double a, double b)
         {
-            return a + b;
+
+            if (a > 0 && b > 0)
+            {
+                c = a + b;
+            }
+            else if (a < 0 && b > 0)
+            {
+                throw new Exception("first parameter is less than 0 ");
+            }
+            else if (a > 0 && b < 0)
+            {
+                throw new Exception("second parameter is less than 0 ");
+            }
+            else
+            {
+                throw new Exception("both parameters are less than 0 ");
+            }
+
+            return c;
         }
 
 
         public double Subtract(double a, double b)
         {
-            return a - b;
+            if (a > 0 && b > 0)
+            {
+                c = a - b;
+            }
+            else if (a < 0 && b > 0)
+            {
+                throw new Exception("first parameter should be greater than 0 ");
+            }
+            else if (a > 0 && b < 0)
+            {
+                throw new Exception("second parameter should be greater than 0 ");
+            }
+            else
+            {
+                throw new Exception("both parameters should be greater than 0 ");
+            }
+
+            return c;
         }
 
         public double Multiply(double a, double b)
         {
-            return a * b;
+            if (a > 0 && b > 0)
+            {
+                c = a * b;
+            }
+            else if (a < 0 && b > 0)
+            {
+                throw new Exception("first parameter should be greater than 0 ");
+            }
+            else if (a > 0 && b < 0)
+            {
+                throw new Exception("second parameter should be greater than 0 ");
+            }
+            else
+            {
+                throw new Exception("both parameters should be greater than 0 ");
+            }
+
+            return c;
         }
 
-        public double Power(double x, double exp)
+        public double Power(double a, double b)
         {
-            return Math.Pow(x, exp);
+            if (a > 0 && b > 0)
+            {
+                c = Math.Pow(a, b);
+            }
+            else if (a < 0 && b > 0)
+            {
+                throw new Exception("first parameter should be greater than 0 ");
+            }
+            else if (a > 0 && b < 0)
+            {
+                throw new Exception("second parameter should be greater than 0 ");
+            }
+            else
+            {
+                throw new Exception("both parameters should be greater than 0 ");
+            }
+
+            return c;
         }
 
-      
         public double Divide(double dividend, double divisor)
         {
-            if (divisor < 0)
+            if (divisor == 0)
             {
-                throw new Exception("You can not divide with minus number");
+                throw new DivideByZeroException("You can not divide with minus number");
             }
+
+            if (dividend < 0)
+            {
+                throw new Exception("Nominator should be greater than 0");
+            }
+
             return dividend / divisor;
+        }
+
+        public double squarRoot(double value)
+        {
+            if (value == 0)
+            {
+                throw new Exception("Value can not be 0");
+            }
+
+            if (value < 0)
+            {
+                throw new Exception("Value should be greater than 0");
+            }
+
+
+            return accumulator += Math.Sqrt(value);
         }
 
 
@@ -49,44 +141,102 @@ namespace Calculator_
         }
         public void clear()
         {
+            if (accumulator == 0)
+            {
+                throw new Exception("It is already clear");
+            }
             accumulator = 0;
         }
 
-        public double Add(double addend)
+        public double Add(double a)
         {
-            accumulator = accumulator + addend;
+            if (a > 0)
+            {
+                accumulator += a;
+            }
+            else if (a < 0)
+            {
+                throw new Exception("Cannot add minus numbers");
+            }
+            else if (a == 0)
+            {
+                throw new Exception("Number should be greater than 0 ");
+            }
+
             return accumulator;
         }
 
 
-        public double Substract(double substractor)
+        public double Substract(double a)
         {
-            accumulator = accumulator - substractor;
+            if (a > 0)
+            {
+                accumulator = accumulator - a;
+            }
+            else if (a < 0)
+            {
+                throw new Exception("Number should be less than 0 ");
+            }
+            else if (a == 0)
+            {
+                throw new Exception("The result of substraction is the same as accumalator ");
+            }
+
+            return accumulator;
+
+        }
+
+        public double Multiply(double a)
+        {
+            if (a > 0)
+            {
+                accumulator *= a;
+            }
+            else if (a < 0)
+            {
+                throw new Exception("Number should be less than 0 ");
+            }
+            else if (a == 0)
+            {
+                throw new Exception("Cannot muliply number by zero ");
+            }
+
             return accumulator;
         }
 
-        public double Multiply(double multiplier)
-        {
-            accumulator = accumulator * multiplier;
-            return accumulator;
-        }
 
-
-        public double Power(double addend)
+        public double Power(double a)
         {
-            if (addend < 0)
+            if (a < 0)
             {
                 throw new Exception("You can not have a power with minus number");
             }
-            accumulator = Math.Pow(accumulator, addend);
+            else if (a > 1000)
+            {
+                throw new IndexOutOfRangeException("Too big number! ");
+            }
+            accumulator = Math.Pow(accumulator, a);
             return accumulator;
         }
 
-        public double Divide(double addend)
+        public double Divide(double a)
         {
-            accumulator = (accumulator / addend);
+            if (a > 0)
+            {
+                accumulator /= a;
+            }
+            else if (a < 0)
+            {
+                throw new Exception("Number should be less than 0 ");
+            }
+            else if (a == 0)
+            {
+                throw new DivideByZeroException("Cannot muliply number by zero ");
+            }
+
             return accumulator;
         }
 
     }
+
 }
